fb25888d74dbd80dbdd0e835a73bafced9835930
ZXTube <ziyadedris@gmail.com>
2022-12-11 08:13:38 +0000

    Shorts now get blocked

--- Code/contentScript.js
+++ Code/contentScript.js
@@ -17,8 +17,10 @@
     }
 
     function manageShorts() {
-        document.head.innerHTML = '<title>Nope</title>';
+        old_document = document.innerHTML;
+
+        document.head.innerHTML = '<title>Nope :)</title>';
         document.body.style = 'background-color: rgb(30, 30, 30); font-family: cursive; text-align: center; color: rgb(200, 200, 200);';
-        document.body.innerHTML = '<p style = "font-size: 80vh; margin: 0px; margin-top: -20vh;">Nope</p><p>There\'s <b>NO</b> way that I\'m going to let you fall into the shorts trap. <a href="history.go(-1)">go back<a></p>';
+        document.body.innerHTML = '<p style = "font-size: 20vw; margin: 0px; margin-top: 5vw; margin-bottom: 5vw">Nope :)</p><p style="font-size:1vw">There\'s <b>NO</b> way that I\'m going to let you fall into the shorts trap.</p>';
     }
 })();
