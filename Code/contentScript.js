b33c1150f5ee5aa4e2e58d7055f39794e5f895c8
ZiyadCodes <82272898+ZXTube@users.noreply.github.com>
2022-12-22 12:28:49 +0000

    Now you can only watch 1 shorts video at once

--- Code/contentScript.js
+++ Code/contentScript.js
@@ -5,7 +5,7 @@
         } else if (obj.type == 'SHORTS_OPENED') {
             manageShorts();
         } else if (obj.type == 'HOMEPAGE_OPENED') {
-            setTimeout(manageHomepage, 1000);
+            setTimeout(manageHomepage, 500);
         }
     });
 
@@ -18,7 +18,7 @@
     }
 
     function manageVideo() {
-        for (let i = 1; i < 15; i++) {
+        for (let i = 1; i < 50; i++) {
             setTimeout(() => {
                 let video_recommendations = document.getElementById('secondary');
                 if (video_recommendations)
@@ -35,25 +35,64 @@
                 let autoplay_video = document.getElementsByClassName('ytp-autonav-endscreen-countdown-overlay')[0];
                 if (autoplay_video)
                     autoplay_video.remove();
-            }, i * 500);
+
+                removeDangerousThings();
+            }, i * 100);
         }
     }
 
     function manageShorts() {
-        document.head.innerHTML = '<title>Nope :)</title>';
-        document.body.style = 'background-color: rgb(30, 30, 30); font-family: cursive; text-align: center; color: rgb(200, 200, 200);';
-        document.body.innerHTML = '<p style = "font-size: 20vw; margin: 0px; margin-top: 5vw; margin-bottom: 5vw">Nope :)</p><p style="font-size:1vw">There\'s <b>NO</b> way that I\'m going to let you fall into the shorts trap.</p>';
+        for (let i = 1; i < 10; i++) {
+            setTimeout(() => {
+                let otherShorts = document.getElementsByTagName('ytd-reel-video-renderer');
+                for (let i = 1; i < otherShorts.length; i++) {
+                    otherShorts[i].remove();
+                }
+
+                removeDangerousThings();
+            }, i * 500);
+        }
     }
 
     function manageHomepage() {
-        for (let i = 1; i < 25; i++) {
+        for (let i = 1; i < 70; i++) {
             setTimeout(() => {
-                let video_recommendations = document.getElementsByTagName('ytd-rich-grid-renderer')[0];
-                if (video_recommendations) {
-                    video_recommendations.innerHTML = 'This place is too dangerous';
-                    video_recommendations.style = "font-family: cursive; font-size: 4vw; text-align: center; width: 80%;position: absolute; top: 45%; color: rgb(200, 200, 200)";
+                let video_recommendations = document.getElementsByTagName('ytd-rich-grid-renderer');
+                if (video_recommendations.length > 0 && video_recommendations[0].classList.contains('style-scope')) {
+                    video_recommendations[0].classList.remove('style-scope');
+                    video_recommendations[0].innerHTML = `<img src="${chrome.runtime.getURL("Resources/Homepage.png")}" style="width:80vw; position: absolute; top: 1px;">`;
+                    video_recommendations[0].style = "font-family: cursive; font-size: 4vw; text-align: center; width: 80%;position: absolute; top: 10%; color: rgb(200, 200, 200)";
                 }
-            }, i * 300);
+
+                removeDangerousThings();
+            }, i * 100);
         }
     }
+
+    function removeDangerousThings() {
+        let thingsOnBottomLeft = document.getElementsByTagName('ytd-guide-section-renderer')
+        if (thingsOnBottomLeft.length > 2) {
+            thingsOnBottomLeft[2].remove();
+            thingsOnBottomLeft[3].remove();
+        }
+
+        let buttonsOnTopLeft = document.getElementById('sections');
+        if (buttonsOnTopLeft) {
+            let shortsButtonGrandparent = buttonsOnTopLeft.children[0]
+            if (shortsButtonGrandparent) {
+                let shortsButtonParent = shortsButtonGrandparent.children[1]
+                if (shortsButtonParent.children.length > 3)
+                    shortsButtonParent.children[1].remove();
+            }
+        }
+
+        let notificationsButton = document.getElementsByTagName('ytd-notification-topbar-button-renderer');
+        if (notificationsButton.length > 0)
+            notificationsButton[0].remove();
+    }
 })();
+
+// QUOTE: I hope you came here concously not subconsously, because if you came here subconsously thank your self for downloading YouShield
+// QUOTE: Watch what you want not what the algorithm wants
+
+// ERRORS: Fix youtube.com/@examplechannel/videos/ problem
